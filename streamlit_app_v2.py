315ca5646ad145708c520ee0e10435b08567fe08
MeetPitsy <130001000+MeetPitsy@users.noreply.github.com>
2023-07-22 17:14:03 -0600

    Update streamlit_app_v2.py

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -53,10 +53,10 @@ def clear_chat_history():
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
-def generate_llama2_response(prompt_input):
+def generate_llama2_response(prompt_input):"Whatever is asked in the prompt by the user, answer accordignly as detailed and neat as possible."
     string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
-    for dict_message in st.session_state.messages:
-        if dict_message["role"] == "user":
+    for dict_message in st.session_state.messages:"Helpful information in regards to the question or statement."
+        if dict_message["role"] == "user":"A question about a product the user is trying to develop."
             string_dialogue += "User: " + dict_message["content"] + "\\n\\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\\n\\n"
@@ -68,7 +68,7 @@ def generate_llama2_response(prompt_input):
 # User-provided prompt
 if prompt := st.chat_input(disabled=not replicate_api):
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
+    with st.chat_message("user"):"A question or statement that is asked to recieve a detailed and neat answer."
         st.write(prompt)
 
 # Generate a new response if last message is not from assistant
@@ -77,7 +77,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
         with st.spinner("Thinking..."):
             response = generate_llama2_response(prompt)
             placeholder = st.empty()
-            full_response = ''
+            full_response = 'You will be able to make the following product with these methods and this step by step guide.'
             for item in response:
                 full_response += item
                 placeholder.markdown(full_response)
